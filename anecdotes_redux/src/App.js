bed7261a11bfe49487fb41ebc06796af8e9215ed
Oula Antere <oula.antere@gmail.com>
2018-11-28 17:13:57 +0200

    Anecdote exercises (and whole part 5) done.

--- anecdotes_redux/src/App.js
+++ anecdotes_redux/src/App.js
@@ -1,27 +1,71 @@
-import React from 'react';
+/* eslint react/prop-types: 0 */
 
+
+import React from 'react'
+
+
+const actionFor = {
+  voting(id) {
+    return{
+      type: 'VOTE',
+      data: { id: id }
+    }
+  },
+  anecdoteCreation(text) {
+    return {
+      type: 'CREATE',
+      data: {content: text}
+    }
+  }
+}
 
 class App extends React.Component {
+
+  vote = (id) => () => {
+    this.props.store.dispatch(
+      actionFor.voting(id)
+    )
+  }
+
+  createAnecdote = (event) => {
+    event.preventDefault()
+    const text = event.target.text.value
+    if (text.length > 0){
+      this.props.store.dispatch(
+        actionFor.anecdoteCreation(text)
+      )
+    }
+  }
+
+  sortAnecdotes = (anecdotes) => {
+    anecdotes.sort((a, b) =>{
+      if (a.votes < b.votes) return 1
+      else if (a.votes > b.votes) return -1
+      else return 0
+    })
+    return anecdotes
+  }
+
   render() {
-    const anecdotes = this.props.store.getState()
+    const anecdotes = this.sortAnecdotes(this.props.store.getState())
     return (
       <div>
         <h2>Anecdotes</h2>
         {anecdotes.map(anecdote=>
           <div key={anecdote.id}>
             <div>
-              {anecdote.content} 
+              {anecdote.content}
             </div>
             <div>
               has {anecdote.votes}
-              <button>vote</button>
+              <button onClick={this.vote(anecdote.id)}>vote</button>
             </div>
           </div>
         )}
         <h2>create new</h2>
-        <form>
-          <div><input /></div>
-          <button>create</button> 
+        <form onSubmit={this.createAnecdote}>
+          <div><input name='text' /></div>
+          <button type='submit'>create</button>
         </form>
       </div>
     )
--- anecdotes_redux/src/reducer.js
+++ anecdotes_redux/src/reducer.js
@@ -20,10 +20,21 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ',state)
-  console.log('action', action)
-  
-  return state
+
+  switch (action.type) {
+  case 'VOTE':{
+    const id = action.data.id
+    const anecdoteToChange = state.find(a => a.id === id)
+    const changedAnecdote = { ...anecdoteToChange, votes: anecdoteToChange.votes + 1}
+    return state.map(anecdote => anecdote.id === id ? changedAnecdote : anecdote)
+  }
+  case 'CREATE':{
+    const newAnecdote = asObject(action.data.content)
+    return [...state,newAnecdote]
+  }
+  default:
+    return state
+  }
 }
 
 export default reducer
