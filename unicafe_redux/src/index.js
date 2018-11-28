5d7a3b7d181617978358acbae63e3c8049c6c1d6
Oula Antere <oula.antere@gmail.com>
2018-11-28 15:56:08 +0200

    Unicafe ready

--- unicafe_redux/src/index.js
+++ unicafe_redux/src/index.js
@@ -1,50 +1,82 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Feedback from './components/Feedback'
-import Statistics from './components/Statistics'
+import {createStore} from 'redux'
+import counterReducer from './counterReducer'
 
-class App extends React.Component {
-  constructor(){
-    super()
+const store = createStore(counterReducer)
 
-    this.fb_classes = {
-
-    }
-
-    this.state = {
-      good: {text: 'Positive', value:0},
-      neutral: {text: 'Neutral', value:0},
-      bad: {text: 'Negative', value:0}
-    }
-  }
+const Statistiikka = (props) => {
+  const good = store.getState().good
+  const neutral = store.getState().neutral
+  const bad = store.getState().bad
+  const palautteita = good + neutral + bad
+  const keskiarvo = (good + -1 * bad) / palautteita
+  const positiivisia = ((good / palautteita) * 100).toFixed(1) + ' %'
 
 
-  render() {
-    const castVote = (voteClass) => () => {
-      this.setState({[voteClass]:
-        {
-          ...this.state[voteClass],
-          value:this.state[voteClass].value + 1
-        }
-      })
-    }
-
-    const buttonValues = {}
-    for (let value in this.state) {
-      buttonValues[value] = {text: this.state[value].text, clickHandler: castVote(value)}
-    }
-
+  if (palautteita === 0) {
     return (
       <div>
-        <Feedback title="How was your experience?" classes={buttonValues}/>
-        <Statistics title="Statistics" values={this.state}/>
+        <h2>statistiikka</h2>
+        <div>ei yhtään palautetta annettu</div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>statistiikka</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>hyvä</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutraali</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>huono</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>keskiarvo</td>
+            <td>{keskiarvo}</td>
+          </tr>
+          <tr>
+            <td>positiivisia</td>
+            <td>{positiivisia}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button onClick={props.nollaa}>nollaa tilasto</button>
+    </div >
+  )
+}
+
+class App extends React.Component {
+  klik = (nappi) => () => {
+    store.dispatch({ type: nappi })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>anna palautetta</h2>
+        <button onClick={this.klik('GOOD')}>hyvä</button>
+        <button onClick={this.klik('NEUTRAL')}>neutraali</button>
+        <button onClick={this.klik('BAD')}>huono</button>
+        <Statistiikka nollaa={this.klik('ZERO')}/>
       </div>
     )
   }
 }
 
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+renderApp()
+store.subscribe(renderApp)
